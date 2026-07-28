15f1eed2a61bcc820a93e986151ccff52d36fc16
kyo0324ma-beep <kyo0324ma@gmail.com>
2026-07-28 13:01:47 +0900

    Create data.js
    Add app data manager

---
+++ js/data.js
@@ -0,0 +1,35 @@
+const appData = {
+
+    // お金
+    cash: 0,
+
+    suica: 0,
+
+    // 収入
+    income: 0,
+
+    salary: 0,
+
+
+    // 履歴
+    history: [],
+
+
+    // カテゴリー別支出
+    spending: {},
+
+
+    // カテゴリー予算
+    budgets: {},
+
+
+    // 貯金目標
+    goal: {
+
+        name: "",
+
+        amount: 0
+
+    }
+
+};
